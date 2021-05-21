69f25247a0d2e729fb5d1f93c274555ce65ad036
DariyaKoroleva <81414458+DariyaKoroleva@users.noreply.github.com>
2021-05-21 13:24:48 +0300

    Add files via upload

---
+++ Characteristic.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace TimetableOfClasses
+{
+    public partial class Characteristic : Form
+    {
+        public Characteristic() => InitializeComponent();
+
+
+
+	}
+}
